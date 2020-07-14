b3f26d37b16df532e582c78129223c2077ce126b
jovanna1616 <jovana.r@vivifyideas.com>
2020-07-14 10:09:09 +0200

    Added paginated todos list.

--- apps/todos/models.py
+++ apps/todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
+from user.models import CustomUser
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField("Date Created", default=datetime.now())
+
+    def __str__(self):
+        return self.title
+
--- apps/todos/views.py
+++ apps/todos/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from todos.models import Todo
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 def todos(request):
-    return HttpResponse("Hello from todos page")
+    todos_list = Todo.objects.all().order_by("-created_at")
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(todos_list, 2)
+
+    try:
+        todos = paginator.page(page_number)
+    except PageNotAnInteger:
+        todos = paginator.page(1)
+    except EmptyPage:
+        todos = paginator.page(paginator.num_pages)
+
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request=request,
+                  template_name="main/todos.html",
+                  context={"todos": todos})
